36690d7291ebf903432ddd25c2917fa8652816bb
Aakarshit Raj <rajaakarshit7@gmail.com>
2025-12-15 02:10:54 +0530

    first commit3

--- expenseTracker/script.js
+++ expenseTracker/script.js
@@ -7,6 +7,40 @@ let showlist = document.getElementById("list");
 let expenseList = [];
 
 addButton.addEventListener("click", addExpense);
+// Allow adding expense on pressing Enter key when focused on add button and use tab to focus on it
+addButton.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault(); // optional
+    addExpense();
+  }
+});
+
+// Allow adding expense on pressing Enter key anywhere on the page
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    addExpense();
+  }
+});
+
+
+
+
+
+function renderExpenses() {
+  showlist.innerHTML = "";
+
+  for (let i = 0; i < expenseList.length; i++) {
+    showlist.innerHTML += `
+      <div>
+        <li>${expenseList[i].text}: ₹${expenseList[i].amount}</li>
+        <span>
+          <button onclick="deleteExpense(${i})">Delete</button>
+        </span>
+      </div>
+    `;
+  }
+  updateTotal(expenseList);
+}
 
 function addExpense() {
   let text = inputText.value;
@@ -17,8 +51,9 @@ function addExpense() {
     return;
   }
   expenseList.push({ text, amount });
-  showlist.innerHTML += `<li>${text}: ₹${amount}</li>`;
-  updateTotal(expenseList);
+  renderExpenses();
+  saveExpenses(); 
+
   inputText.value = "";
   amountInput.value = "";
   return;
@@ -27,6 +62,26 @@ function updateTotal(expenseList) {
   let total = 0;
   for (let i = 0; i < expenseList.length; i++) {
     total += expenseList[i].amount;
-    totalDisplay.innerText = `${total}`;
+  }
+  totalDisplay.innerText = total;
+}
+function deleteExpense(index) {
+  expenseList.splice(index, 1);
+  renderExpenses();
+  saveExpenses(); 
+}
+
+function saveExpenses() {
+  localStorage.setItem("mydata", JSON.stringify(expenseList));
+}
+
+function loadExpenses() {
+  const savedExpenses = localStorage.getItem("mydata");
+  if (savedExpenses) {
+    expenseList = JSON.parse(savedExpenses);
+    renderExpenses();
   }
 }
+
+// Call this once when page loads
+loadExpenses();
